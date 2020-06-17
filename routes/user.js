81097cd063ba1caca206f61a98cf448eceb8d7d4
Johirrim <65472303+Johirrim@users.noreply.github.com>
2020-06-16 19:48:06 -0500

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +17,40 @@ router.get('/contacto', function(req, res, next) {
 /* GET nosotros page. */
 router.get('/nosotros', function(req, res, next) {
   res.render('user/nosotros', { title: 'FIGURADOS Y MALLAS' });
+});
+router.post('/email', async (req,res)=>{
+  const {name, email, phone, message}=req.body;
+  contentHTML = `
+  <h1>Información del remitente</h1>
+  <ul>
+      <li>Nombre: ${name}</li>
+      <li>Email: ${email}</li>
+      <li>Teléfono: ${phone}</li>
+  </ul>
+  <h1>Mensaje</h1>
+  <p>${message}</p>
+`;
+
+let transporter = nodemailer.createTransport({
+  host: 'mail.figuradosymallas.com',
+  port: 587,
+  secure: false,
+  auth: {
+      user: 'info@figuradosymallas.com',
+      pass: 'Robotina2016'
+  },
+  tls: {
+      rejectUnauthorized: false
+  }
+});
+
+let info = await transporter.sendMail({
+  from: email, // sender address,
+  to: 'info@figuradosymallas.com',
+  subject: 'Contacto desde figuradosymallas.com ✅',
+  html: contentHTML
+})
+  res.redirect('/');  
 });
 
 module.exports = router;
